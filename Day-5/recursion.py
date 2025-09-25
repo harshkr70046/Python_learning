327687fbfd5900f12833e8fc5383bf9f13113018
Harsh Kumar <harshkrsonbarsha98@gmail.com>
2025-09-25 23:18:36 +0530

    Changes

--- Day-5/recursion.py
+++ Day-5/recursion.py
@@ -16,3 +16,18 @@ def febonachi(num):
     return febonachi(num-1)+febonachi(num-2)
 
 print(f"Fibonachi of {num}th is = ",febonachi(3))
+
+# print series of febonachi 
+def feb(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return feb(n-1) + feb(n-2)
+
+def series_febonachi(num):
+    for i in range(num):
+        print(feb(i), end=", ")
+
+series_febonachi(10)
+
