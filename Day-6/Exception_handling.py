867a62688c7642515ec97997fbadcbc48b03e587
Harsh Kumar <harshkrsonbarsha98@gmail.com>
2025-09-27 02:08:41 +0530

    Changes

---
+++ Day-6/Exception_handling.py
@@ -0,0 +1,18 @@
+a = input("Enter the number = ")
+print(f"multiplication table of {a}")
+try:
+    for i in range(10):
+        print(f"{int(a)}X{i+1}={int(a)*(i+1)}")
+
+except:
+    print("invalid input")
+print("end of program")
+
+try:
+    num = int(input("Enter index = "))
+    li = [7,9]
+    print(li[num])
+except ValueError:
+    print("You have not entered int value.")
+except IndexError:
+    print("index out of range")
